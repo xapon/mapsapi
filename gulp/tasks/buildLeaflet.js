09d739e17e65006c0a3e03ff670b5a2a1086a8e5
vincent <cherebedov.sergey@gmail.com>
2016-08-25 19:47:04 +0700

    Parameter for remove files from leaflet

--- gulp/tasks/buildLeaflet.js
+++ gulp/tasks/buildLeaflet.js
@@ -1,11 +1,16 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
+var util = require('gulp-util');
 
 var config = require('../../app/config.js');
 var deps = require('../deps')(config);
 
+var removedFiles = util.env.removeFromLeaflet ? util.env.removeFromLeaflet.split(',') : [];
+
 gulp.task('buildLeaflet', function () {
-    return gulp.src(deps.getJSFiles({source: 'leaflet'}))
+    return gulp.src(deps.getJSFiles({source: 'leaflet'}).filter(function (item) {
+        return removedFiles.indexOf(item.split('/').pop()) === -1;
+    }))
         .pipe(concat('leaflet-src.js'))
         .pipe(gulp.dest('node_modules/leaflet/dist/'));
 });
